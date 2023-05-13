2086e2c1ad993c86f842e232adfc5fcd7899bcd7
Lans12345 <olanalans12345@gmail.com>
2023-05-13 10:20:44 +0800

    added landing screen

--- lib/screens/auth/landing_screen.dart
+++ lib/screens/auth/landing_screen.dart
@@ -1,3 +1,5 @@
+import 'package:beepet/utils/colors.dart';
+import 'package:beepet/widgets/text_widget.dart';
 import 'package:flutter/material.dart';
 
 class LandingScreen extends StatelessWidget {
@@ -5,6 +7,44 @@ class LandingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: GestureDetector(
+        onTap: () {},
+        child: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/images/back1.png'),
+                fit: BoxFit.cover),
+          ),
+          child: Column(
+            children: [
+              const Expanded(
+                child: SizedBox(),
+              ),
+              Center(
+                child: Image.asset('assets/images/logo.png', height: 300),
+              ),
+              const SizedBox(
+                height: 180,
+              ),
+              Container(
+                width: double.infinity,
+                height: 40,
+                decoration: BoxDecoration(color: Colors.black.withOpacity(0.3)),
+                child: Center(
+                  child: TextBold(
+                      text: 'Tap anywhere to continue',
+                      fontSize: 18,
+                      color: primary),
+                ),
+              ),
+              const SizedBox(
+                height: 75,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
