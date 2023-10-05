dee40a83b3feae81d8128ec622535462501c0b5b
Lans12345 <olanalans12345@gmail.com>
2023-10-05 18:53:08 +0800

    fixed overflows

--- lib/screens/auth/forgot_password/forgot_password_screen_2.dart
+++ lib/screens/auth/forgot_password/forgot_password_screen_2.dart
@@ -21,91 +21,95 @@ class ForgotPasswordScreen2 extends StatelessWidget {
           image: DecorationImage(
               image: AssetImage('assets/images/back1.png'), fit: BoxFit.cover),
         ),
-        child: Column(
-          children: [
-            Container(
-              width: double.infinity,
-              height: 180,
-              color: primary,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Image.asset('assets/images/logo.png', height: 150),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          TextBold(
-                              text: 'Online', fontSize: 24, color: Colors.blue),
-                          TextBold(
-                              text: 'PetLoc',
-                              fontSize: 28,
-                              color: Colors.green),
-                        ],
-                      )
-                    ],
-                  ),
-                  TextBold(
-                      text: 'Online Vets loved by Pets',
-                      fontSize: 14,
-                      color: Colors.black),
-                ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                width: double.infinity,
+                height: 180,
+                color: primary,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Image.asset('assets/images/logo.png', height: 150),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            TextBold(
+                                text: 'Online',
+                                fontSize: 24,
+                                color: Colors.blue),
+                            TextBold(
+                                text: 'PetLoc',
+                                fontSize: 28,
+                                color: Colors.green),
+                          ],
+                        )
+                      ],
+                    ),
+                    TextBold(
+                        text: 'Online Vets loved by Pets',
+                        fontSize: 14,
+                        color: Colors.black),
+                  ],
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 80,
-            ),
-            TextBold(
-                text: 'Forgot Password', fontSize: 24, color: Colors.black),
-            const SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 30, right: 30),
-              child: Card(
-                elevation: 3,
-                child: Container(
-                  width: double.infinity,
-                  height: 100,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.grey),
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                  child: Center(
-                    child: TextRegular(
-                        text:
-                            'A verification code has been\nsent to your contact number',
-                        fontSize: 18,
-                        color: Colors.grey),
+              const SizedBox(
+                height: 80,
+              ),
+              TextBold(
+                  text: 'Forgot Password', fontSize: 24, color: Colors.black),
+              const SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 30, right: 30),
+                child: Card(
+                  elevation: 3,
+                  child: Container(
+                    width: double.infinity,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: Center(
+                      child: TextRegular(
+                          text:
+                              'A verification code has been\nsent to your contact number',
+                          fontSize: 18,
+                          color: Colors.grey),
+                    ),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            Center(
-                child: TextFieldWidget(
-                    labelColor: Colors.black,
-                    label: 'Enter Verification Code',
-                    controller: codeController)),
-            const SizedBox(
-              height: 30,
-            ),
-            Center(
-                child: ButtonWidget(
-                    radius: 100,
-                    color: Colors.teal[600]!.withOpacity(0.5),
-                    label: 'Continue',
-                    onPressed: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) => ResetPasswordScreen()));
-                    })),
-          ],
+              const SizedBox(
+                height: 30,
+              ),
+              Center(
+                  child: TextFieldWidget(
+                      labelColor: Colors.black,
+                      label: 'Enter Verification Code',
+                      controller: codeController)),
+              const SizedBox(
+                height: 30,
+              ),
+              Center(
+                  child: ButtonWidget(
+                      radius: 100,
+                      color: Colors.teal[600]!.withOpacity(0.5),
+                      label: 'Continue',
+                      onPressed: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => ResetPasswordScreen()));
+                      })),
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/auth/forgot_password/reset_password_screen.dart
+++ lib/screens/auth/forgot_password/reset_password_screen.dart
@@ -23,75 +23,80 @@ class ResetPasswordScreen extends StatelessWidget {
           image: DecorationImage(
               image: AssetImage('assets/images/back1.png'), fit: BoxFit.cover),
         ),
-        child: Column(
-          children: [
-            Container(
-              width: double.infinity,
-              height: 180,
-              color: primary,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Image.asset('assets/images/logo.png', height: 150),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          TextBold(
-                              text: 'Online', fontSize: 24, color: Colors.blue),
-                          TextBold(
-                              text: 'PetLoc',
-                              fontSize: 28,
-                              color: Colors.green),
-                        ],
-                      )
-                    ],
-                  ),
-                  TextBold(
-                      text: 'Online Vets loved by Pets',
-                      fontSize: 14,
-                      color: Colors.black),
-                ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                width: double.infinity,
+                height: 180,
+                color: primary,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Image.asset('assets/images/logo.png', height: 150),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            TextBold(
+                                text: 'Online',
+                                fontSize: 24,
+                                color: Colors.blue),
+                            TextBold(
+                                text: 'PetLoc',
+                                fontSize: 28,
+                                color: Colors.green),
+                          ],
+                        )
+                      ],
+                    ),
+                    TextBold(
+                        text: 'Online Vets loved by Pets',
+                        fontSize: 14,
+                        color: Colors.black),
+                  ],
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 80,
-            ),
-            TextBold(text: 'Reset Password', fontSize: 24, color: Colors.black),
-            const SizedBox(
-              height: 50,
-            ),
-            Center(
-                child: TextFieldWidget(
-                    labelColor: Colors.black,
-                    label: 'Enter Password',
-                    controller: passwordController)),
-            const SizedBox(
-              height: 20,
-            ),
-            Center(
-                child: TextFieldWidget(
-                    labelColor: Colors.black,
-                    label: 'Confirm Password',
-                    controller: confirmpasswordController)),
-            const SizedBox(
-              height: 30,
-            ),
-            Center(
-                child: ButtonWidget(
-                    radius: 100,
-                    color: Colors.teal[600]!.withOpacity(0.5),
-                    label: 'Reset Password',
-                    onPressed: () {
-                      showToast('Password resetted succesfully!');
-                      Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) => LoginScreen()));
-                    })),
-          ],
+              const SizedBox(
+                height: 80,
+              ),
+              TextBold(
+                  text: 'Reset Password', fontSize: 24, color: Colors.black),
+              const SizedBox(
+                height: 50,
+              ),
+              Center(
+                  child: TextFieldWidget(
+                      labelColor: Colors.black,
+                      label: 'Enter Password',
+                      controller: passwordController)),
+              const SizedBox(
+                height: 20,
+              ),
+              Center(
+                  child: TextFieldWidget(
+                      labelColor: Colors.black,
+                      label: 'Confirm Password',
+                      controller: confirmpasswordController)),
+              const SizedBox(
+                height: 30,
+              ),
+              Center(
+                  child: ButtonWidget(
+                      radius: 100,
+                      color: Colors.teal[600]!.withOpacity(0.5),
+                      label: 'Reset Password',
+                      onPressed: () {
+                        showToast('Password resetted succesfully!');
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => LoginScreen()));
+                      })),
+            ],
+          ),
         ),
       ),
     );
