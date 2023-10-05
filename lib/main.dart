6e0abba358ac7f5e678d6100bb12d03ed2ae52d5
Lans12345 <olanalans12345@gmail.com>
2023-10-05 18:55:41 +0800

    fixed overflows

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    name: 'beepet',
+    name: 'BEEPET',
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
